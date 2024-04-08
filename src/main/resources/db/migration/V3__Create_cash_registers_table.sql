ea6ab26031d424159db87efef5597ae7b451f694
IImures <vlad.kalinchenko215@gmail.com>
2024-04-08 14:16:09 +0200

    Updated Flyway migration

--- src/main/resources/db/migration/V3__Create_cash_registers_table.sql
+++
@@ -1,7 +0,0 @@
-create table cash_registers (
-                                id bigserial not null,
-                                description varchar(255) not null,
-                                general_info varchar(255) not null,
-                                name varchar(255) not null,
-                                primary key (id)
-)
--- src/main/resources/db/migration/V5__Create_characteristic_table.sql
+++ src/main/resources/db/migration/V5__Create_characteristic_table.sql
@@ -2,7 +2,7 @@ create table characteristic (
                                 id bigserial not null,
                                 key varchar(255) not null,
                                 value varchar(255) not null,
-                                cash_register_id bigint,
+                                item_id bigint,
                                 primary key (id),
-                                constraint cash_register_fk foreign key (cash_register_id) references cash_registers
+                                constraint item_fk foreign key (item_id) references items
 )
