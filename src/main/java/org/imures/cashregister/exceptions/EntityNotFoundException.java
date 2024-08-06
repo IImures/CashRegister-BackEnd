117ae6715f8b335bd19c8fc4ac935f4a8ec396f9
Vladyslav Kalynchenko <vladyslav.kalynchenko@gmail.com>
2024-08-06 23:15:11 +0200

    Feature: Deleted unnecessary EntityNotFoundException

--- src/main/java/org/imures/cashregister/exceptions/EntityNotFoundException.java
+++
@@ -1,7 +0,0 @@
-//package org.imures.cashregister.exceptions;
-//
-//public class EntityNotFoundException extends RuntimeException{
-//    public EntityNotFoundException(String message) {
-//        super(message);
-//    }
-//}
