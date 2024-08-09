a5e715c448f06bdeff00d5e0578ed612241f94a2
Vladyslav Kalynchenko <vladyslav.kalynchenko@gmail.com>
2024-08-09 19:24:21 +0200

    Feature: Temporarily removed CORS policy

--- src/main/java/org/imures/cashregister/catalog/controller/SubCatalogController.java
+++ src/main/java/org/imures/cashregister/catalog/controller/SubCatalogController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/v1/sub-catalog")
+@CrossOrigin(origins = "*")
 public class SubCatalogController {
 
     private final SubCatalogService subCatalogService;
