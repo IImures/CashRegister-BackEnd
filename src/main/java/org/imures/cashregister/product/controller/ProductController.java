fe3e773cbc1a3ebd2f413f222c8e47ee06ca7dd6
Vladyslav Kalynchenko <vladyslav.kalynchenko@gmail.com>
2024-08-09 19:23:08 +0200

    Feature: Temporarily removed CORS policy, modified GetImage it returns image in base64

--- src/main/java/org/imures/cashregister/product/controller/ProductController.java
+++ src/main/java/org/imures/cashregister/product/controller/ProductController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -16,15 +17,17 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.Base64;
 
 @RestController
 @RequestMapping("api/v1/product")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class ProductController {
 
     private final ProductService productService;
 
-    @GetMapping
+    @GetMapping(    "sub-catalog/{subCatalogId}")
     public ResponseEntity<Page<ProductResponse>> getPagedCatalogs(
             @RequestParam(
                     value = "page",
@@ -40,11 +43,12 @@ public class ProductController {
                     value = "sort",
                     required = false,
                     defaultValue = "id"
-            ) String sortBy
+            ) String sortBy,
+            @PathVariable Long subCatalogId
     ){
         if(limit > 150 || limit <= 0) throw new TooManyEntitiesRequestedException("Limit must be less then 150 or higher then 0");
         Pageable pageRequest = PageRequest.of(page, limit, Sort.by(sortBy));
-        Page<ProductResponse> response = productService.findAll(pageRequest);
+        Page<ProductResponse> response = productService.findAll(pageRequest, subCatalogId);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
@@ -71,13 +75,17 @@ public class ProductController {
     }
 
     @GetMapping(path = "{productId}/image")
-    public ResponseEntity<byte[]> getImage(
+    public ResponseEntity<String> getImage(
             @PathVariable Long productId
     ) {
         byte[] image = productService.getImageBytes(productId);
+        String base64Image = "data:image/png;base64," + Base64.getEncoder().encodeToString(image);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.TEXT_PLAIN);
         return ResponseEntity.status(HttpStatus.OK)
-                .contentType(MediaType.valueOf("image/png"))
-                .body(image);
+                .headers(headers)
+                .body(base64Image);
     }
 
     @DeleteMapping(path = "/{productId}")
